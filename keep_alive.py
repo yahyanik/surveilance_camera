b26673dac68591526cefb5ca321a5743824409cd
yahya nikouei <snikoue1@binghamton.edu>
2021-08-22 21:57:59 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,9 +1,11 @@
 
 import requests
+import time
 
-url = "'0.0.0.0':8111/stay_alive"
+url = "0.0.0.0:8111/stay_alive"
 
-_ = requests.get(url, stream=True)
+time.sleep(3)
 
-
-
+while True:
+    _ = requests.get(url, stream=True)
+    time.sleep(10)
