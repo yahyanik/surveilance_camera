113cbce605079327ce3d5741e2d08a717f672bd8
yahya nikouei <snikoue1@binghamton.edu>
2021-08-29 18:31:29 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,19 @@
 
 import requests
 import time
+from utils import sms
+import datetime
 
 url = "localhost:8111/stay_alive"
 
 time.sleep(3)
 
 while True:
-    _ = requests.get(url, stream=True)
+    try:
+        _ = requests.get(url, stream=True)
+    except:
+        text = f'Camera activity: Camera not reachable.' \
+                           f'time is {datetime.datetime.now()}' \
+        sms.send(text)
+        
     time.sleep(10)
