7a84ee8566e64a04309e49798224bcba0fec089d
yahya nikouei <snikoue1@binghamton.edu>
2021-08-29 23:05:26 -0400

    Update run_app.bash

--- run_app.bash
+++ run_app.bash
@@ -4,6 +4,15 @@ if [ ! -d "./env" ]
 then
     echo "Virtual env does not exists. "
     echo "Installing packages... "
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt-get -y install libcblas-dev
+    sudo apt-get -y install libhdf5-dev
+    sudo apt-get -y install libhdf5-serial-dev
+    sudo apt-get -y install libatlas-base-dev
+    sudo apt-get -y install libjasper-dev 
+    sudo apt-get -y install libqtgui4 
+    sudo apt-get -y install libqt4-test
     python3 -m pip install --upgrade pip
     python3 -m pip install --user virtualenv
     python3 -m venv env
