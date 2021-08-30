5c26aaea244fd89df477d1cd5a119bce459f5fd5
yahya nikouei <snikoue1@binghamton.edu>
2021-08-29 22:02:50 -0400

    Update run_app.bash

--- run_app.bash
+++ run_app.bash
@@ -4,6 +4,7 @@ if [ ! -d "./env" ]
 then
     echo "Virtual env does not exists. "
     echo "Installing packages... "
+    python3 -m pip install --upgrade pip
     python3 -m pip install --user virtualenv
     python3 -m venv env
     source ./env/bin/activate
